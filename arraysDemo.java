511c3bd3be98efd4ba210426b4814d02753591b2
Erimsah Olut <115551882+Erimsaholut@users.noreply.github.com>
2023-01-17 22:18:01 +0300

    Add files via upload

---
+++ arraysDemo.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Erim";
+        //System.out.println(myName);
+
+        String[] names = {"Ali", "Bugra", "Ceylin", "Deniz"};  //String names[] also works
+
+        for (int i = 0; names.length > i; i++) {
+            System.out.println(names[i]);
+        }
+
+        System.out.println("------------------");
+
+        for (String name:names) {   //name in names
+            System.out.println(name);
+        }
+
+        System.out.println("------------------");
+
+        double[] mylist;
+
+
+    }
+}
