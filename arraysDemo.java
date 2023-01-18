4c1c619fbfb7572ec6fc02153fed771c86819e55
Erimsah Olut <115551882+Erimsaholut@users.noreply.github.com>
2023-01-18 11:20:04 +0300

    Update arraysDemo.java

--- arraysDemo.java
+++ arraysDemo.java
@@ -11,14 +11,44 @@ public class arraysDemo {
 
         System.out.println("------------------");
 
-        for (String name:names) {   //name in names
+        for (String name : names) {   //name in names
             System.out.println(name);
         }
 
         System.out.println("------------------");
 
-        double[] mylist;
+        double[] mylist = {1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 9.0};
+        double total = 0;
+        double maks = mylist[0];
 
+        for (double number : mylist) {                //for(number in mylist ) print(number)
+            System.out.println(number);
+            total += number;
 
+            if (number > maks)
+                maks = number;
+
+        }
+        System.out.println("Total: " + total);
+        System.out.println("Maks: " + maks);
+
+        System.out.println("------------------");
+        // MULTİDİMENSİONAL ARRAYS
+        String[][] sehirler = new String[3][3];
+        sehirler[0][0] = "manis";
+        sehirler[0][1] = "izmir";
+        sehirler[0][2] = "istan";
+        sehirler[1][0] = "akhisar";
+        sehirler[1][1] = "turgutlu";
+        sehirler[1][2] = "saruhanlı";
+        sehirler[2][0] = "bornova";
+        sehirler[2][1] = "aliaga";
+        sehirler[2][2] = "menemen";
+
+        for(int i=0;3>i;i++){
+            for(int j=0;3>j;j++){
+                System.out.println(sehirler[i][j]);
+            }
+        }
     }
 }
