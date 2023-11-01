8433144257db8b9216f6d1b3ea078d1729061283
Erimsah Olut <115551882+Erimsaholut@users.noreply.github.com>
2023-11-01 15:55:17 +0300

    Add files via upload

---
+++ patika_dev_java102/src/hackerRank/q1.java
@@ -0,0 +1,21 @@
+package hackerRank;
+
+import java.util.Scanner;
+
+public class q1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int b, h;
+        b = input.nextInt();
+        h = input.nextInt();
+        if (b <= 0 || h <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            System.exit(0);
+        }
+        System.out.println(h*b);
+
+
+    }
+
+}
